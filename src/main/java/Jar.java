2e37bd58f1cbf0e7a815b3b2e0e2a2a0bc1b23c6
Mike Gentry <mgentry007@gmail.com>
2017-03-25 22:07:48 -0700

    Changes from automated testing - mostly text

--- src/main/java/Jar.java
+++ src/main/java/Jar.java
@@ -2,36 +2,38 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Jar {
-  private String jarItem;
-  private int maxItems;
+  private String itemName;
+  private int maximumNumber;
   private int actual;
   private int attempts = 0;
   Scanner scanner = new Scanner(System.in);
   Random random = new Random();
   
   public String promptForItem() {
-    System.out.print("Administrator, what type of item is in the jar? ");
+    System.out.println("ADMINISTRATOR INPUT:");
+    System.out.print("What type of item is in the jar? ");
     String itemInput = scanner.nextLine();
-    jarItem = itemInput;
-    return jarItem;
+    itemName = itemInput;
+    return itemName;
   }
   
   public int totalPossibleItems() {
-    System.out.printf("Administrator, how many total %s can fit in the jar? ",
-                     jarItem);
+    System.out.printf("What is the maximum amount of %s? ",
+                     itemName);
     int total = scanner.nextInt();
-    maxItems = total;
-    return maxItems;
+    maximumNumber = total;
+    return maximumNumber;
   }
   
   public int actualNumber() {
-    actual = (random.nextInt(maxItems)) + 1;
+    actual = (random.nextInt(maximumNumber)+ 1);
     return actual;
   }
   
   public void promptForGuess() {
-    System.out.printf("Player: How many %s are in the jar?%nPick a number between 1 and %d: ",
-                     jarItem, maxItems);
+    System.out.println("PLAYER:");
+    System.out.printf("How many %s are in the jar?%nPick a number between 1 and %d: ",
+                     itemName, maximumNumber);
     int guess = scanner.nextInt();
     while (guess != actual) {
       System.out.print("Your guess was incorrect. Please try again: ");
